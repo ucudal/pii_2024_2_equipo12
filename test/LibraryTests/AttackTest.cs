0b5324ca04008b45ca7367a6632051e821f8967a
luletteiriarte <carolina.iriarte@correo.ucu.edu.uy>
2024-11-18 11:19:38 -0300

    cambio en test

--- test/LibraryTests/AttackTest.cs
+++ test/LibraryTests/AttackTest.cs
@@ -1,117 +1,82 @@
 ﻿using NUnit.Framework;
 using Poke.Clases;
 using Type = Poke.Clases.Type;
-
 namespace LibraryTests;
 
-[TestFixture]
-public class AttackTest
-{
-    
-    /*  Como jugador, quiero atacar en mi turno y hacer daño basado en la efectividad de los tipos de Pokémon.
-        Criterios de aceptación:
-        El jugador puede seleccionar un ataque que inflige daño basado en la efectividad de tipos.
-        El sistema aplica automáticamente la ventaja o desventaja del tipo de ataque. */
-    
-    [Test]
-    public void TestDamageBasedOnTypeEffectiveness()
+    [TestFixture]
+    public class AttackTest
     {
-        // Crear Pokémon
-        Pokemon firePokemon = new Pokemon("Charmander", 100, 1, null, Type.PokemonType.Fire);
-        Pokemon waterPokemon = new Pokemon("Squirtle", 100, 1, null, Type.PokemonType.Water);
-        Pokemon plantPokemon = new Pokemon("Bulbasaur", 100, 1, null, Type.PokemonType.Plant);
-
-        // Crear ataques
-        NormalAttack fireAttack = new NormalAttack("Fire Blast", 40,  Type.PokemonType.Fire, false);
-        NormalAttack waterAttack = new NormalAttack("Water Gun", 40, Type.PokemonType.Water, false );
-        NormalAttack plantAttack = new NormalAttack("Vine Whip", 40, Type.PokemonType.Plant, false );
-
-        // Aplicar ventaja de tipo: Fire > Plant
-        double expectedFireDamage = fireAttack.CalculateDamage(plantPokemon);
-        plantPokemon.RecibeDamage(expectedFireDamage);
-        Assert.That(plantPokemon.GetHp(), Is.EqualTo(20));
-
-        // Aplicar ventaja de tipo: Water > Fire
-        double expectedWaterDamage = waterAttack.CalculateDamage(firePokemon);
-        firePokemon.RecibeDamage(expectedWaterDamage);
-        Assert.That(firePokemon.GetHp(), Is.EqualTo(20));
-
-        // Aplicar desventaja de tipo: Water < Plant
-        double expectedPlantDamage = plantAttack.CalculateDamage(waterPokemon);
-        waterPokemon.RecibeDamage(expectedPlantDamage);
-        Assert.That(waterPokemon.GetHp(), Is.EqualTo(20));
-    }
-}
-
-
-[TestFixture]
-public class AvailableAttacksTest
-{
-    private OriginalTrainer jugador;
-    private Pokemon pokemon;
-    private Attack ataqueBasico;
-    private Attack ataqueEspecial;
-    private Battle battle; // Declaramos la instancia de Battle
-
-    [SetUp]
-    public void SetUp()
-    {
-        ataqueBasico = new Attack("Ataque Básico", 10, Type.PokemonType.Water, false);
-        ataqueEspecial = new Attack("Ataque Especial", 20, Type.PokemonType.Electric, true);
-        pokemon = new Pokemon("Pikachu", 100, 10, "1", Type.PokemonType.Electric);
-        pokemon.AttackList = new List<Attack> { ataqueBasico, ataqueEspecial };
-        jugador = new OriginalTrainer("Jugador1", pokemon);
-
-        // Creación de la instancia de Battle
-        battle = new Battle(pokemon, new Pokemon("Charmander", 100, 10, "2", Type.PokemonType.Fire));
-    }
-
-    [Test]
-    public void MostrarAtaquesDisponibles_Test()
-    {
-        // Verificar que se muestran los ataques para el turno actual
-        List<Attack> ataquesDisponibles = pokemon.AttackList;
-        Assert.That(ataquesDisponibles, Does.Contain(ataqueBasico));
-        Assert.That(ataquesDisponibles, Does.Contain(ataqueEspecial));
-    }
-
-    /*[Test]
-    public void AtaqueEspecial_RestriccionDeTurnos_Test()
-    {
-        // Simulamos el turno 1
-        battle.Turn = 1;
-        battle.PlayTurn(jugador, new Trainer("Jugador2", new Pokemon("Charmander", 100, 10, "3", Type.PokemonType.Fire)));
-        // Ahora verificamos
-        if (battle.ActualTurn == 1)
-        {
-            bool puedeUsarEspecialTurno1 = false;
-            Assert.That(puedeUsarEspecialTurno1, Is.False, "No debe poder usar ataque especial en el primer turno");
-        }
-        else
-        {
-            bool puedeUsarEspecialTurno1 = true;
-        }
-
-        // Simulamos el turno 2
-        battle.Turn = 2;
-        ataqueBasico = new Attack("Ataque Básico", 10, Type.PokemonType.Water, false);
-        ataqueEspecial = new Attack("Ataque Especial", 20, Type.PokemonType.Electric, true);
-        battle.PlayTurn(jugador, new Trainer("Jugador2", new Pokemon("Charmander", 100, 10, "3", Type.PokemonType.Fire, new List<Attack> { ataqueBasico, ataqueEspecial })));
         
-        // Ahora verificamos si puede usar el ataque especial en el segundo turno
-        if (battle.ActualTurn == 1)
+        /*  Como jugador, quiero atacar en mi turno y hacer daño basado en la efectividad de los tipos de Pokémon.
+            Criterios de aceptación:
+            El jugador puede seleccionar un ataque que inflige daño basado en la efectividad de tipos.
+            El sistema aplica automáticamente la ventaja o desventaja del tipo de ataque. */
+        
+        [Test]
+        public void TestDamageBasedOnTypeEffectiveness()
         {
-            bool puedeUsarEspecialTurno2 = false;
-        }
-        else
-        {
-            bool puedeUsarEspecialTurno2 = true;
-            Assert.That(puedeUsarEspecialTurno2, Is.True, "Debe poder usar ataque especial en el segundo turno");
+            // Crear Pokémon
+            Pokemon firePokemon = new Pokemon("Charmander", 100, 1, null, Type.PokemonType.Fire);
+            Pokemon waterPokemon = new Pokemon("Squirtle", 100, 1, null, Type.PokemonType.Water);
+            Pokemon plantPokemon = new Pokemon("Bulbasaur", 100, 1, null, Type.PokemonType.Plant);
+
+            // Crear ataques
+            NormalAttack fireAttack = new NormalAttack("Fire Blast", 40,  Type.PokemonType.Fire, false);
+            NormalAttack waterAttack = new NormalAttack("Water Gun", 40, Type.PokemonType.Water, false );
+            NormalAttack plantAttack = new NormalAttack("Vine Whip", 40, Type.PokemonType.Plant, false );
+
+            // Aplicar ventaja de tipo: Fire > Plant
+            double expectedFireDamage = fireAttack.CalculateDamage(plantPokemon);
+            plantPokemon.RecibeDamage(expectedFireDamage);
+            Assert.That(plantPokemon.GetHp(), Is.EqualTo(20));
+
+            // Aplicar ventaja de tipo: Water > Fire
+            double expectedWaterDamage = waterAttack.CalculateDamage(firePokemon);
+            firePokemon.RecibeDamage(expectedWaterDamage);
+            Assert.That(firePokemon.GetHp(), Is.EqualTo(20));
+
+            // Aplicar desventaja de tipo: Water < Plant
+            double expectedPlantDamage = plantAttack.CalculateDamage(waterPokemon);
+            waterPokemon.RecibeDamage(expectedPlantDamage);
+            Assert.That(waterPokemon.GetHp(), Is.EqualTo(20));
         }
     }
-    */
-}
 
+
+    [TestFixture]
+    public class AvailableAttacksTest
+    {
+        private OriginalTrainer jugador;
+        private Pokemon pokemon;
+        private Attack ataqueBasico;
+        private Attack ataqueEspecial;
+        private Battle battle; // Declaramos la instancia de Battle
+
+        [SetUp]
+        public void SetUp()
+        {
+            ataqueBasico = new Attack("Ataque Básico", 10, Type.PokemonType.Water, false);
+            ataqueEspecial = new Attack("Ataque Especial", 20, Type.PokemonType.Electric, true);
+            pokemon = new Pokemon("Pikachu", 100, 10, "1", Type.PokemonType.Electric);
+            pokemon.AttackList = new List<Attack> { ataqueBasico, ataqueEspecial };
+            jugador = new OriginalTrainer("Jugador1", pokemon);
+
+            // Creación de la instancia de Battle
+            battle = new Battle(pokemon, new Pokemon("Charmander", 100, 10, "2", Type.PokemonType.Fire));
+        }
+
+        [Test]
+        public void MostrarAtaquesDisponibles_Test()
+        {
+            // Verificar que se muestran los ataques para el turno actual
+            List<Attack> ataquesDisponibles = pokemon.AttackList;
+            Assert.That(ataquesDisponibles, Does.Contain(ataqueBasico));
+            Assert.That(ataquesDisponibles, Does.Contain(ataqueEspecial));
+        }
+    }
+
+
+    [TestFixture]
     public class IniciateBattleWithWaitListOpponentTest
     {
         private OriginalTrainer jugador1;
@@ -165,7 +130,7 @@ public class AvailableAttacksTest
         }
     }
     
-    
+
     [TestFixture]
     public class Pick6PokemonTest
     {
@@ -224,13 +189,46 @@ public class AvailableAttacksTest
         }
     }
     
+
+    [TestFixture]
     public class PlayersWaitListTest
     {
-        /* Como entrenador, quiero ver la lista de jugadores esperando por un oponente.
-            Criterios de aceptación:
-            En la pantalla se ve la lista de jugadores que se unieron a la lista de espera. */
+        private OriginalTrainer jugador1;
+        private OriginalTrainer jugador2;
+        private WaitList waitList;
+
+        [SetUp]
+        public void SetUp()
+        {
+            jugador1 = new OriginalTrainer("Jugador1", new Pokemon("Pikachu", 100, 10, "1", Type.PokemonType.Electric));
+            jugador2 = new OriginalTrainer("Jugador2", new Pokemon("Charmander", 100, 10, "2", Type.PokemonType.Fire));
+            waitList = new WaitList();
+        }
+
+        [Test]
+        public void VerListaDeEspera_Test()
+        {
+            // Capturar la salida de la consola para verificar el mensaje
+            var consoleOutput = new StringWriter();
+            Console.SetOut(consoleOutput);
+
+            // Agregar jugadores a la lista de espera
+            waitList.AddToWaitList(jugador1);
+            waitList.AddToWaitList(jugador2);
+
+            // Verificar que los jugadores fueron agregados a la lista de espera
+            var playersInWaitList = waitList.CheckIn();
+            Assert.That(playersInWaitList, Contains.Item(jugador1), "El jugador1 debería estar en la lista de espera.");
+            Assert.That(playersInWaitList, Contains.Item(jugador2), "El jugador2 debería estar en la lista de espera.");
+
+            // Verificar el mensaje de confirmación
+            string output = consoleOutput.ToString();
+            Assert.That(output, Contains.Substring("Jugador1 ha sido añadido a la lista de espera"), "El mensaje de confirmación debería indicar que el jugador1 fue agregado a la lista de espera.");
+            Assert.That(output, Contains.Substring("Jugador2 ha sido añadido a la lista de espera"), "El mensaje de confirmación debería indicar que el jugador2 fue agregado a la lista de espera.");
+        }
     }
     
+ 
     [TestFixture]
     public class PokemonsHealthTest
     {
@@ -281,6 +279,9 @@ public class AvailableAttacksTest
             Assert.That(pokemon1.Hp, Is.EqualTo(0), "Los puntos de vida de Pikachu no deberían ser negativos.");
         }
     }
+
+
+    [TestFixture]
     public class TurnInfoTest
     {
         private OriginalTrainer jugador;
@@ -362,13 +363,41 @@ public class AvailableAttacksTest
             }
         }
     }
+
+    [TestFixture]
     public class WaitTest
     {
-        /* Como entrenador, quiero unirme a la lista de jugadores esperando por un oponente.
-            Criterios de aceptación:
-            El jugador recibe un mensaje confirmando que fue agregado a la lista de espera. */
-    }
+        private OriginalTrainer jugador;
+        private WaitList waitList;
 
+        [SetUp]
+        public void SetUp()
+        {
+            jugador = new OriginalTrainer("Jugador1", new Pokemon("Pikachu", 100, 10, "1", Type.PokemonType.Electric));
+            waitList = new WaitList();
+        }
+
+        [Test]
+        public void UnirseAListaDeEspera_Test()
+        {
+            // Capturar la salida de la consola para verificar el mensaje
+            var consoleOutput = new StringWriter();
+            Console.SetOut(consoleOutput);
+
+            // Agregar jugador a la lista de espera
+            waitList.AddToWaitList(jugador);
+
+            // Verificar que el jugador fue agregado a la lista de espera
+            Assert.That(waitList.CheckIn(), Contains.Item(jugador), "El jugador debería estar en la lista de espera.");
+
+            // Verificar el mensaje de confirmación
+            string output = consoleOutput.ToString();
+            Assert.That(output, Contains.Substring("Jugador1 ha sido añadido a la lista de espera"), "El mensaje de confirmación debería indicar que el jugador fue agregado a la lista de espera.");
+        }
+    }
+    
+
+    [TestFixture]
     public class WinBattleTest
     {
         private OriginalTrainer jugador;
