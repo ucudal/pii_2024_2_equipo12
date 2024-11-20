912b1bc55f99b62cb1dc87e2b7ac8c36b3eeae42
luletteiriarte <carolina.iriarte@correo.ucu.edu.uy>
2024-11-20 10:21:57 -0300

    Command Change POkemon

--- src/Library/ChatBot/Commands/BattleCommands/PossiblePlays/ChangePokemonCommand.cs
+++ src/Library/ChatBot/Commands/BattleCommands/PossiblePlays/ChangePokemonCommand.cs
@@ -5,29 +5,22 @@ namespace Ucu.Poo.DiscordBot.Commands;
 
 /// <summary>
 /// Esta clase implementa el comando 'changepokemon' del bot. Este comando permite
-/// al jugador cambiar su Pokémon actual por otro del catálogo.
+/// al jugador cambiar su Pokémon actual por otro de los seleccionados.
 /// </summary>
 // ReSharper disable once UnusedType.Global
 public class ChangePokemonCommand : ModuleBase<SocketCommandContext>
 {
     /// <summary>
     /// Implementa el comando 'changepokemon'. Este comando permite al jugador
-    /// cambiar su Pokémon actual por otro del catálogo.
+    /// cambiar su Pokémon actual por otro de los seleccionados.
     /// </summary>
     [Command("changePokemon")]
-    [Summary("Permite al usuario cambiar su Pokémon actual por otro del catálogo")]
+    [Summary("Permite al usuario cambiar su Pokémon actual por otro de los seleccionados.")]
     // ReSharper disable once UnusedMember.Global
     public async Task ExecuteAsync(string pokemonName)
     {
-        if (Enum.TryParse<PokemonCatalog.Catalog>(pokemonName, true, out var catalogEntry))
-        {
-            var pokemon = PokemonCatalog.CreatePokemon(catalogEntry);
-            // Aquí puedes agregar la lógica para asignar el nuevo Pokémon al jugador
-            await ReplyAsync($"Has cambiado a {pokemon.Name}");
-        }
-        else
-        {
-            await ReplyAsync("El nombre del Pokémon no es válido. Por favor, intenta nuevamente.");
-        }
+        string displayName = CommandHelper.GetDisplayName(Context);
+        await ReplyAsync(Facade.Instance.ChangePokemon(displayName, pokemonName));
+        
     }
 }
